83e0ea835d9ae5dbbd460966b48d3b298657fc98
Elena <zenkinaelena78@gmail.com>
2019-02-17 22:40:43 +0300

    Задание 8

---
+++ Zenkina_Elena_Task08/Task/DynamicArray.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class DynamicArray<T> where T : new()
+    {
+        T[] dynArray;
+        int length;
+        int capacity;
+
+        /// <summary>
+        /// Конструктор без параметров создаёт массив емкостью 8 элементов.
+        /// </summary>
+        public DynamicArray()
+        {
+            dynArray = new T[ 8 ];
+            length = 0;
+            capacity = 8;
+        }
+
+        /// <summary>
+        /// Конструктор массива с заданной емкостью.
+        /// </summary>
+        public DynamicArray(uint capacity)
+        {
+            dynArray = new T[ capacity ];
+            length = 0;
+            this.capacity = (int)capacity;
+        }
+
+        /// <summary>
+        /// Конструктор, который в качестве параметра принимает массив.
+        /// </summary>
+        public DynamicArray(DynamicArray<T> sourceArray)
+        {
+            dynArray = new T[sourceArray.Length];
+            sourceArray.dynArray.CopyTo(dynArray, 0);
+            length = sourceArray.Length;
+            capacity = length;
+        }
+
+        /// <summary>
+        /// Индексатор, позволяющий работать с элементом с указанным номером.
+        /// </summary>
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= capacity)
+                {
+                    throw new ArgumentOutOfRangeException("Index", $"Индекс {index} не должен выходить за границу массива");
+                }
+                return dynArray [ index ];
+            }
+            set
+            {
+                if (index < 0 || index >= capacity)
+                {
+                    throw new ArgumentOutOfRangeException("Index", $"Индекс {index} не должен выходить за границу массива");
+                }
+                dynArray [ index ] = value;
+                // При необходимости увеличиваем длину заполненной части массива
+                length = length <= index ? ++index : length;
+            }
+        }
+
+        private void Resize(int capacity)
+        {
+            Array.Resize(ref dynArray, capacity);
+            this.capacity = capacity;
+        }
+
+        public void Add(T item)
+        {
+            if (length == capacity)
+            {
+                Resize(capacity == 0 ? 1 : capacity * 2);
+            }
+
+            dynArray[length++] = item;
+        }
+
+        public void AddRange(DynamicArray<T> addArray)
+        {
+            var addArrayLen = addArray.Length;
+
+            if (addArray == null || addArrayLen == 0)
+            {
+                return;
+            }
+
+            if (length + addArrayLen > capacity)
+            {
+                Resize(length + addArrayLen);
+            }
+
+            for (int i = 0; i < addArrayLen; i++)
+            {
+                dynArray[length + i] = addArray[i];
+            }
+
+            length += addArrayLen;
+        }
+
+        /// <summary>
+        /// Удаление указанного элемента.
+        /// </summary>
+        public bool Remove(T item)
+        {
+            if (dynArray == null || length == 0)
+            {
+                return false;
+            }
+
+            int index = Array.IndexOf(dynArray, item);
+            if (index == -1)
+            {
+                return false;
+            }
+
+            for (int i = index; i < length - 1; i++)
+            {
+                dynArray[i] = dynArray[i + 1];
+            }
+
+            dynArray[ --length ] = default(T);
+            return true;
+        }
+
+        /// <summary>
+        /// Добавление элемента в произвольную позицию массива.
+        /// </summary>
+        public bool Insert(int index, T item )
+        {
+            if (index < 0 || index >= capacity)
+            {
+                throw new ArgumentOutOfRangeException("Index", $"Индекс {index} не должен выходить за границу массива");
+            }
+            if (length == capacity)
+            {
+                Resize(capacity == 0 ? 1 : capacity * 2);
+            }
+
+            for (int i = length; i > index; i--)
+            {
+                dynArray[i] = dynArray[i - 1];
+            }
+            dynArray[index] = item;
+
+            length++;
+            return true;
+        }
+
+        /// <summary>
+        /// Получение длины заполненной части массива.
+        /// </summary>
+        public int Length => length;
+
+        /// <summary>
+        /// Получение ёмкости массива - количества всех элементов.
+        /// </summary>
+        public int Capacity => capacity;
+
+    }
+}
---
+++ Zenkina_Elena_Task08/Task/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dynArray = new DynamicArray<int>();
+            Output("Конструктор без параметров создаёт массив емкостью 8 элементов:", dynArray);
+
+            var myArray = new DynamicArray<int>(10);
+            Output("Конструктор массива с заданной емкостью 10:", myArray);
+
+            myArray[0] = 0;
+            myArray[1] = 1;
+            myArray[2] = 1;
+            myArray[3] = 2;
+            myArray[4] = 3;
+            myArray[5] = 5;
+            myArray[6] = 8;
+            myArray[7] = 13;
+            myArray[8] = 21;
+            myArray[9] = 34;
+
+            dynArray = new DynamicArray<int>(myArray);
+            Output("Конструктор, который в качестве параметра принимает массив (с числами Фибоначчи):", dynArray);
+
+            dynArray.Add(55);
+            Output("Добавление одного элемента в конец массива:", dynArray);
+
+            myArray = new DynamicArray<int>(3);
+            myArray[0] = 89;
+            myArray[1] = 144;
+            myArray[2] = 233;
+            dynArray.AddRange(myArray);
+            Output("Добавление в конец массива элементов другого массива:", dynArray);
+
+            if (dynArray.Remove(34))
+            {
+                Output("Удаление элемента, содержащего значение 34:", dynArray);
+            }
+
+            dynArray.Insert(9, 34);
+            Output("Добавление числа 34 на позицию с индексом 9:", dynArray);
+
+            // Попытка вставить элемент с индексом 99
+            try
+            {
+                dynArray.Insert(99, 34);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Нажмите Enter для выхода из программы.");
+            Console.ReadLine();
+
+        }
+
+        private static void Output(string message, DynamicArray<int> dynArray)
+        {
+            Console.WriteLine(message);
+            if (dynArray.Length > 0)
+            {
+                for (int i = 0; i < dynArray.Length; i++)
+                {
+                    Console.Write("{0}\t", dynArray[i]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Длина заполненной части массива: {dynArray.Length}, емкость массива: {dynArray.Capacity}.");
+            Console.WriteLine();
+        }
+    }
+}
